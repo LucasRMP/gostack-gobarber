b5e7fff4269baedc782017456a329b5293527080
Lucas Pessone <lucas.rmagalhaes@gmail.com>
2020-01-27 21:55:52 -0200

    Test user creation

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 
+import User from './app/models/User';
+
 const router = new Router();
 
-router.get('/', (req, res) => {
-  return res.json({ Hello: 'World' });
+router.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Lucas Pessone',
+    email: 'lucas.rmagalhaes@gmail.com',
+    password_hash: 'oid091eoiqid0912jdoq',
+  });
+
+  return res.json(user);
 });
 
 export default router;
