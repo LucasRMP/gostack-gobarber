b53fece7e0c6d190450a593991ebf133b076a87e
Lucas Pessone <lucas.rmagalhaes@gmail.com>
2020-01-28 20:23:14 -0200

    feat: Add file upload and avatar store and relation

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMiddleware from './app/middlewares/auth';
 
+import multerConfig from './config/multer';
+
 const router = new Router();
+const upload = multer(multerConfig);
 
 router.post('/sessions', SessionController.store);
 router.post('/users', UserController.store);
@@ -13,5 +18,7 @@ router.post('/users', UserController.store);
 router.use(authMiddleware);
 
 router.put('/users', UserController.update);
+
+router.post('/files', upload.single('file'), FileController.store);
 
 export default router;
