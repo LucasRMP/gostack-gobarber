b7a155477777a26229cbc3b091641e7c874e867f
Lucas Pessone <lucas.rmagalhaes@gmail.com>
2020-02-18 19:45:02 -0300

    chore(web): add non auth pages structure and styles

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,10 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as yup from 'yup';
+
+import logo from '~/assets/images/logo.svg';
+
+const validationSchema = yup.object().shape({
+  email: yup
+    .string('Email must be a string')
+    .email('Email must be valid')
+    .required('Email is required'),
+  password: yup.string().required('Password is required'),
+});
 
 function SignIn() {
+  const handleSubmit = data => {
+    console.tron.log(data);
+  };
+
   return (
-    <div>
-      <h1>Sign In</h1>
-    </div>
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form onSubmit={handleSubmit} schema={validationSchema} noValidate>
+        <Input type="email" placeholder="Email" name="email" />
+        <Input type="password" placeholder="Password" name="password" />
+
+        <button type="submit">Login</button>
+        <Link to="/register">Sign Up</Link>
+      </Form>
+    </>
   );
 }
 
--- web/src/pages/SignUp/index.js
+++ web/src/pages/SignUp/index.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
+import * as yup from 'yup';
+
+import logo from '~/assets/images/logo.svg';
+
+const validationSchema = yup.object().shape({
+  name: yup.string('Name must be a string').required('Name is required'),
+  email: yup
+    .string('Email must be a string')
+    .email('Email must be valid')
+    .required('Email is required'),
+  password: yup
+    .string()
+    .min(6, 'Password must be at least 6 characters long')
+    .required('Password is required'),
+});
 
 function SignUp() {
+  const handleSubmit = data => {
+    console.tron.log(data);
+  };
+
   return (
-    <div>
-      <h1>Sign Up</h1>
-    </div>
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <Form onSubmit={handleSubmit} schema={validationSchema} noValidate>
+        <Input name="name" placeholder="Full name" />
+        <Input name="email" type="email" placeholder="Email" />
+        <Input name="password" type="password" placeholder="Password" />
+
+        <button type="submit">Create account</button>
+        <Link to="/">Alredy has an account? Sign in</Link>
+      </Form>
+    </>
   );
 }
 
