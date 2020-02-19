d785d023fa001f82c40b73fddbae6323d2bf05f4
Lucas Pessone <lucas.rmagalhaes@gmail.com>
2020-02-19 20:07:27 -0300

    style(web): add header component and styles

---
+++ web/src/components/Header/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { Container, Content, Profile } from './styles';
+
+import logo from '~/assets/images/logo-purple-small.svg';
+
+function Header() {
+  return (
+    <Container>
+      <Content>
+        <nav>
+          <img src={logo} alt="GoBarber" />
+          <Link to="/dashboard">Dashboard</Link>
+        </nav>
+
+        <aside>
+          <Profile>
+            <div>
+              <strong>Lucas Pessone</strong>
+              <Link to="/profile">My Profile</Link>
+            </div>
+            <img
+              src="https://api.adorable.io/avatars/50/abott@adorable.pngCopy to Clipboard"
+              alt="https://api.adorable.io/avatars/50/abott@adorable.pngCopy to Clipboard"
+            />
+          </Profile>
+        </aside>
+      </Content>
+    </Container>
+  );
+}
+
+export default Header;
