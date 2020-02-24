48ba1be0a4d708972451ca162891cc7cdaf1425e
LucasRMP <lucas.rmagalhaes@gmail.com>
2020-02-24 12:07:15 -0300

    style(mobile): finish the signin page stlye

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,17 +1,47 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
 
-import Input from '~/components/Input';
-import Button from '~/components/Button';
 import Background from '~/components/Background';
-// import { Container } from './styles';
+
+import logo from '~/assets/logo.png';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles';
 
 function SignIn() {
   return (
     <Background>
-      <Text>SignIn</Text>
-      <Input style={{ marginTop: 30 }} icon="call" placeholder="Nome" />
-      <Button>Hello</Button>
+      <Container>
+        <Image source={logo} />
+
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Email"
+          />
+          <FormInput
+            icon="lock-outline"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Senha"
+            secureTextEntry
+          />
+
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Não possui uma conta? Cadastrar</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
